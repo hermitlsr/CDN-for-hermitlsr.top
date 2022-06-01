213f4595dd7b808c03393792b8584bf7e33e0d97
偊由 <84276102+hermitlsr@users.noreply.github.com>
2022-06-01 20:55:20 +0800

    Add files via upload

---
+++ js/printpage.js
@@ -0,0 +1,12 @@
+  <script language="javascript">
+	function printpage(){
+	  bdhtml=window.document.body.innerHTML;//获取当前页的html代码
+	  sprnstr="<!--startprint-->"; //设置打印开始区域
+	  eprnstr="<!--endprint-->"; //设置打印结束区域
+	  prnhtml=bdhtml.substring(bdhtml.indexOf(sprnstr)+17); //从开始代码向后取html
+	  prnhtml=prnhtml.substring(0,prnhtml.indexOf(eprnstr));//从结束代码向前取html
+	  window.document.body.innerHTML=prnhtml;
+	  window.print();
+	  window.document.body.innerHTML=bdhtml;
+	}
+  </script>
